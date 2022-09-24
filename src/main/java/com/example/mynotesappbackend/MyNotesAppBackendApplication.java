43d571c0280bd902a3a678432f9fb0b775826404
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2022-09-24 00:42:00 -0300

    commit inicial

---
+++ src/main/java/com/example/mynotesappbackend/MyNotesAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mynotesappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNotesAppBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyNotesAppBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/mynotesappbackend/MyNotesAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mynotesappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNotesAppBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
